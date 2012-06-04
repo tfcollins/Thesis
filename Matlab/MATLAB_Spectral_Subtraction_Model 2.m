4c4ecdaa9a890ffbf3960b1bbe55e061cb22e8ec
Travis Collins <travisfcollins@gmail.com>
2012-06-04 15:44:59 -0400

    Inital FDE working, time to integrate

--- Matlab/MATLAB_Spectral_Subtraction_Model 2.m
+++ Matlab/MATLAB_Spectral_Subtraction_Model 2.m
@@ -1,7 +1,7 @@
 %Spectral Subtraction Implementation Using Pulse Shapes in Frequency Domain
-%TFC
+
 % Half the length of the srrc pulse
-size_d = 10;
+size = 10;
 
 % amount of information to be sent
 dataLength = 4e3;
@@ -18,7 +18,7 @@ samples = 10;   % Number of samples
 Beta_rolloff=.5;    % roll off factor for the srrc pulse
 
 % pulse_srrc is the srrc pulse
-pulse_srrc = 10*srrc(size_d,Beta_rolloff,samples);
+pulse_srrc = 10*srrc(size,Beta_rolloff,samples);
 
 % x_data is the signal convolved with the pulse shape
 x_data = conv(pulse_srrc,upsamp_data);
@@ -30,21 +30,21 @@ t = 1/Fs:1/Fs:length(x_data)/Fs;
 x_Modul = x_data.*cos(2*pi*t*fo1)';
 
 % pulse_hamming is a hamming pulse in the time domain
-pulse_hamming = hamming(size_d*samples*2+1);
+pulse_hamming = hamming(size*samples*2+1);
 
 % y_data is the interfering signal
 y_data = conv(pulse_srrc,upsamp_data2);
 
 % Carrier frequency for interfering signal
-fo2 = 8e4-15e3;
+fo2 = 8e4-0e3;
 
-y_Modul = 0.1*y_data.*cos(2*pi*t*fo2)';
+y_Modul = 2*y_data.*cos(2*pi*t*fo2)';
 
 % z is the combination of x_data and y_data providing the interference
 z = x_Modul+y_Modul;
 
 % Here the noise is added to the signal
-m = awgn(z,.1);
+m = awgn(z,10,'measured');
 
 % this defines the precision of the fft
 precision = 20*dataLength;
@@ -53,7 +53,7 @@ precision = 20*dataLength;
 EST_data = upsample(randi([0,1], dataLength,1),10);
 EST_y_data = conv(pulse_srrc,EST_data);
 EST_y_Modul = EST_y_data.*cos(2*pi*t*fo2)';
-EST_y_Subtract = .5*EST_y_Modul;
+EST_y_Subtract = 5*EST_y_Modul;
 
 % Y_fft and M_fft are the fourier transforms of the estimated singal and
 % combined signal
@@ -67,42 +67,12 @@ Omega = angle(fft(x_Modul,precision));
 Y_psd = abs(Y_fft);
 M_psd = abs(M_fft);
 
-%plot(Y_psd.*conj(Y_psd)/precision);
-% figure(4)
-% plot(abs(fft(y_Modul)));
-% hold on;
-% plot(abs(fft(EST_y_Modul*power)),'r');
-% hold off;
-% figure(5)
-
-% M=11;%Taps
-% y=zeros(1,length(l));
-% for i=(M-1)/2+1:length(l)-(M-1)/2
-%     y(i)=0;
-%     for k=-(M-1)/2:(M-1)/2
-%         y(i)=y(i)+l(i+k);
-%     end
-%     y(i)=y(i)/M;
-% end
-% M=11;%Taps
-% y2=zeros(1,length(l));
-% for i=(M-1)/2+1:length(l)-(M-1)/2
-%     y2(i)=0;
-%     for k=-(M-1)/2:(M-1)/2
-%         y2(i)=y(i)+l2(i+k);
-%     end
-%     y2(i)=y(i)/M;
-% end
-
-
 % Alpha and Beta are the values for the musical noise
-Alpha_MN = 2;  % values > 10
+Alpha_MN = 30;  % values > 10
 Beta_MN = 0.2; % values betweeen 0.05 and 0.2;
 
 % Here the spectral subtraction takes place
 X_EST_psd = M_psd - Alpha_MN*Y_psd;
-%X_EST_psd = abs(fft(y_Modul,precision))-Alpha_MN*Y_psd;
-X_EST_psd_save=X_EST_psd;
 
 % These loops reduce the impact of musical noise on the signal
 for i = 1:length(X_EST_psd)
@@ -117,34 +87,13 @@ for i = 1:length(X_EST_psd)
     end
 end
 
-%Plots
-% power=4;
-% figure(3)
-% subplot(4,1,1)
-% plot(abs(fft(EST_y_Modul*power)));
-% ylim([0 10000]);
-% subplot(4,1,2)
-% plot(abs(fft(y_Modul)));
-% ylim([0 10000]);
-% subplot(4,1,3)
-% plot(abs(fft(y_Modul))-abs(fft(EST_y_Modul*power)),'r');
-% ylim([0 10000]);
-% subplot(4,1,4)
-% plot(X_EST_psd);
-% ylim([0 10000]);
-
-
 % Here X_SS is converted back into the frequency domain and time domain
 X_fft = (X_EST_psd).*exp(1i*Omega);
 x_SS = ifft(X_fft,precision);
 
-
-
 % The signal is modulated back down to base band
 t2 = 1/Fs:1/Fs:length(x_SS)/Fs;
 X_BB = 2*x_SS.*cos(2*pi*t2*fo1)';
-
-
 
 % A filter is applied to remove any unwanted information outside the
 % desired signal
@@ -157,28 +106,6 @@ X_filt = filter(h,1,X_BB);
 
 % x_SS_data is retrieved signal
 x_SS_data = 2*downsample(conv(X_filt, pulse_srrc),10)/100;
-
-
-%Equalize
-% output o f channel
-n=30;
-% length of equalizer -ˆ’ 1
-delta=30;
-% u s e d e l a y <=n * l e n g t h ( b )
-p=length ( data )- delta ;
-R=toeplitz(x_SS_data(n+1:p),x_SS_data(n+1:-1:1));
-% b u i l d m a tr i x R
-data=data';
-S=data( n+1-delta:p-delta)' ;
-% and v e c t o r S
-f=inv(R'*R)*R'* S;
-% calculate equalizer f
-Jmin=S'*S-S'*R*inv(R'*R)*R'*S;
-% Jmin f o r t h i s f and d e l t a
-x_SS_data=filter(f,1,x_SS_data) ;
-
-
-
 
 numErrors = 0;
 
@@ -193,7 +120,7 @@ for i=51:length(data)+50
     end
 end
 
-BitError = numErrors/length(data)
+BitError = numErrors/length(data);
 
 
 % Here the process is plotted
@@ -202,28 +129,24 @@ subplot(5,1,1),
 plot(upsamp_data),
 title('Original Data');
 subplot(5,1,2),
-plot(abs(fft(x_Modul,precision))),
+plot(abs(fft(x_Modul))),
 title('Desired Signal');
 subplot(5,1,3),
-plot(M_psd),
-title('Combined Signals');
+plot(abs(M_fft)),
 hold on
-plot(abs(fft(y_Modul,precision)),'r');
-ylim([0 3000]);
-hold off
+plot(abs(fft(y_Modul)),'r')
+hold off;
+title('Combined Signals');
 subplot(5,1,4),
-plot(X_EST_psd),%abs(X_fft)
+plot(abs(X_fft)),
 title('Subtracted result');
-ylim([0 4000]);
 subplot(5,1,5),
-hold on;
-plot(abs(fft(x_Modul,precision)));
-plot(abs(fft(
-title('Original vs Subtracted');
+plot(real(x_SS_data(51:dataLength+50))),
+title('Time Domain Data w/ Pulse Shape');
 
 % This figure shows the retrieved signal in blue and the orriginal in red
 figure(2)
-plot(data,'r*')
+plot(data,'r.')
 hold on
 plot(real(x_SS_data(51:dataLength+50)),'.')
 title('Comparison of Original and Retrieved Data');
--- Matlab/MATLAB_Spectral_Subtraction_Model_2.m
+++ Matlab/MATLAB_Spectral_Subtraction_Model_2.m
@@ -36,15 +36,15 @@ pulse_hamming = hamming(size_d*samples*2+1);
 y_data = conv(pulse_srrc,upsamp_data2);
 
 % Carrier frequency for interfering signal
-fo2 = 8e4-15e3;
+fo2 = 8e4-1e3;
 
-y_Modul = y_data.*cos(2*pi*t*fo2)';
+y_Modul = 1.5*y_data.*cos(2*pi*t*fo2)';
 
 % z is the combination of x_data and y_data providing the interference
 z = x_Modul+y_Modul;
 
 % Here the noise is added to the signal
-m = awgn(z,.01);
+m = awgn(z,.1);
 
 % this defines the precision of the fft
 precision = 20*dataLength;
@@ -53,7 +53,7 @@ precision = 20*dataLength;
 EST_data = upsample(randi([0,1], dataLength,1),10);
 EST_y_data = conv(pulse_srrc,EST_data);
 EST_y_Modul = EST_y_data.*cos(2*pi*t*fo2)';
-EST_y_Subtract = 5*EST_y_Modul;
+EST_y_Subtract = 1.5*EST_y_Modul;
 
 % Y_fft and M_fft are the fourier transforms of the estimated singal and
 % combined signal
@@ -67,59 +67,42 @@ Omega = angle(fft(x_Modul,precision));
 Y_psd = abs(Y_fft);
 M_psd = abs(M_fft);
 
+%plot(Y_psd.*conj(Y_psd)/precision);
+% figure(4)
+% plot(abs(fft(y_Modul)));
+% hold on;
+% plot(abs(fft(EST_y_Modul*power)),'r');
+% hold off;
+% figure(5)
 
-%Plots
-power=4;
-figure(3)
-subplot(4,1,1)
-plot(abs(fft(EST_y_Modul*power)));
-ylim([0 10000]);
-subplot(4,1,2)
-plot(abs(fft(y_Modul)));
-ylim([0 10000]);
-subplot(4,1,3)
-plot(abs(fft(y_Modul))-abs(fft(EST_y_Modul*power)),'r');
-ylim([0 10000]);
+% M=11;%Taps
+% y=zeros(1,length(l));
+% for i=(M-1)/2+1:length(l)-(M-1)/2
+%     y(i)=0;
+%     for k=-(M-1)/2:(M-1)/2
+%         y(i)=y(i)+l(i+k);
+%     end
+%     y(i)=y(i)/M;
+% end
+% M=11;%Taps
+% y2=zeros(1,length(l));
+% for i=(M-1)/2+1:length(l)-(M-1)/2
+%     y2(i)=0;
+%     for k=-(M-1)/2:(M-1)/2
+%         y2(i)=y(i)+l2(i+k);
+%     end
+%     y2(i)=y(i)/M;
+% end
 
-figure(4)
-plot(abs(fft(y_Modul)));
-hold on;
-plot(abs(fft(EST_y_Modul*power)),'r');
-hold off;
-figure(5)
-%plot(cumsum(abs(fft(y_Modul))-abs(fft(EST_y_Modul*power))),'g');
-l=abs(fft(y_Modul));
-l2=abs(fft(EST_y_Modul*power));
-
-M=11;%Taps
-y=zeros(1,length(l));
-for i=(M-1)/2+1:length(l)-(M-1)/2
-    y(i)=0;
-    for k=-(M-1)/2:(M-1)/2
-        y(i)=y(i)+l(i+k);
-    end
-    y(i)=y(i)/M;
-end
-M=11;%Taps
-y2=zeros(1,length(l));
-for i=(M-1)/2+1:length(l)-(M-1)/2
-    y2(i)=0;
-    for k=-(M-1)/2:(M-1)/2
-        y2(i)=y(i)+l2(i+k);
-    end
-    y2(i)=y(i)/M;
-end
-
-
-plot(l-l2);
-break;
 
 % Alpha and Beta are the values for the musical noise
-Alpha_MN = 30;  % values > 10
+Alpha_MN = 12;  % values > 10
 Beta_MN = 0.2; % values betweeen 0.05 and 0.2;
 
 % Here the spectral subtraction takes place
 X_EST_psd = M_psd - Alpha_MN*Y_psd;
+%X_EST_psd = abs(fft(y_Modul,precision))-Alpha_MN*Y_psd;
+X_EST_psd_save=X_EST_psd;
 
 % These loops reduce the impact of musical noise on the signal
 for i = 1:length(X_EST_psd)
@@ -134,11 +117,22 @@ for i = 1:length(X_EST_psd)
     end
 end
 
+%Plots
+% power=4;
+% figure(3)
+% subplot(4,1,1)
+% plot(abs(fft(EST_y_Modul*power)));
+% ylim([0 10000]);
+% subplot(4,1,2)
+% plot(abs(fft(y_Modul)));
+% ylim([0 10000]);
+% subplot(4,1,3)
+% plot(abs(fft(y_Modul))-abs(fft(EST_y_Modul*power)),'r');
+% ylim([0 10000]);
+% subplot(4,1,4)
+% plot(X_EST_psd);
+% ylim([0 10000]);
 
-subplot(4,1,4)
-plot(X_EST_psd);
-ylim([0 10000]);
-break
 
 % Here X_SS is converted back into the frequency domain and time domain
 X_fft = (X_EST_psd).*exp(1i*Omega);
@@ -181,7 +175,7 @@ f=inv(R'*R)*R'* S;
 % calculate equalizer f
 Jmin=S'*S-S'*R*inv(R'*R)*R'*S;
 % Jmin f o r t h i s f and d e l t a
-x_SS_data=filter(f,1,x_SS_data) ;
+%x_SS_data=filter(f,1,x_SS_data) ;
 
 
 
@@ -208,17 +202,27 @@ subplot(5,1,1),
 plot(upsamp_data),
 title('Original Data');
 subplot(5,1,2),
-plot(abs(fft(x_Modul))),
+plot(abs(fft(x_Modul,precision))),
 title('Desired Signal');
 subplot(5,1,3),
-plot(abs(M_fft)),
+plot(M_psd),
 title('Combined Signals');
+hold on
+plot(abs(fft(y_Modul,precision)),'r');
+ylim([0 3000]);
+hold off
 subplot(5,1,4),
-plot(abs(X_fft)),
+plot(X_EST_psd),%abs(X_fft)
 title('Subtracted result');
+ylim([0 4000]);
 subplot(5,1,5),
-plot(real(x_SS_data(51:dataLength+50))),
-title('Time Domain Data w/ Pulse Shape');
+hold on;
+plot(abs(fft(x_Modul,precision)));
+plot(X_EST_psd,'r');
+plot(abs(abs(fft(x_Modul,precision))-X_EST_psd),'g');
+ylim([0 4000]);
+hold off;
+title('Original vs Subtracted');
 
 % This figure shows the retrieved signal in blue and the orriginal in red
 figure(2)
